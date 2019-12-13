6dccf4b31a72086ce61ae53cc2b121731ad22fb4
David Carlisle <d.p.carlisle@gmail.com>
2019-12-13 18:31:08 +0000

    initial setup

---
+++ build.lua
@@ -0,0 +1,56 @@
+#!/usr/bin/env texlua
+
+
+module = "ltxcmds"
+
+installfiles = {
+'*.sty',
+'*.dtx','*.pdf'
+}
+
+
+textfiles = {"README.md"}
+unpackfiles = {'ltxcmds.dtx'}
+
+packtdszip  = true
+
+maxprintline=10000
+checkruns = 2
+
+tdslocations={
+"doc/latex/oberdiek/ltxcmds.pdf",
+"source/latex/oberdiek/ltxcmds.dtx",
+"tex/generic/oberdiek/ltxcmds.sty",
+}
+
+function update_tag(file,content,tagname,tagdate)
+
+local tagpattern="(%d%d%d%d[-/]%d%d[-/]%d%d) v(%d+[.])(%d+)"
+local oldv,newv
+if tagname == 'auto' then
+  local i,j,olddate,a,b
+  i,j,olddate,a,b= string.find(content, tagpattern)
+  if i == nil then
+    print('OLD TAG NOT FOUND')
+    return content
+  else
+    print ('FOUND: ' .. olddate .. ' v' .. a .. b )
+    oldv = olddate .. ' v' .. a .. b
+    newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
+    print('USING OLD TAG: ' .. oldv)
+    print('USING NEW TAG: ' .. newv)
+    content=string.gsub(content,"{Version}{" .. oldv,'##OLDV##')
+    content=string.gsub(content,string.gsub(oldv,"[-/]", "[-/]"),newv)
+    content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content = string.gsub(content,
+        '%% \\end{History}',
+	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
+    return content
+  end
+else
+  error("only automatic tagging supported")
+end
+
+end
+
+
